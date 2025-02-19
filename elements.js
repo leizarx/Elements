a3eb69dbb9ddd84d7aa0ece3d7f8c55fe8a08777
Raziel Ramirez Rodriguez <140014470+leizarx@users.noreply.github.com>
2025-02-19 09:34:39 -0600

    Update elements.js

--- elements.js
+++ elements.js
@@ -3,65 +3,65 @@ function generarHashNumerico(longitud) {
   for (let i = 0; i < longitud; i++) {hash += Math.floor(Math.random() * 10);}
   console.log('Hash:', hash);
 }
-
 generarHashNumerico(5); 
 
-  $(document).keydown(function(event) {
-  if (event.ctrlKey && event.key === "|") {
-      var FILLL = localStorage.getItem('FILLL');
+$(document).keydown(function(event) {
+    if (event.ctrlKey && event.key === " ") {
+        var FILLL = localStorage.getItem('FILLL');
+        activeElements();
+        event.preventDefault();
+    }
+});
 
-      activeElements();
-      event.preventDefault();
-  }
-}); 
 function activeElements() {
-  var elementos = document.querySelectorAll('input, select, button');
-  elementos.forEach(function(elemento) {
-      var randomColor = getRandomColor();
-      console.groupCollapsed('%c' + elemento.getAttribute('id'), 'color: ' + randomColor);
-      console.log('%cId: ' + elemento.getAttribute('id'), 'color: ' + randomColor);
-      console.log('%cValue: ' + elemento.getAttribute('value'), 'color: ' + randomColor);
-      console.log('%cRequired: ' + elemento.getAttribute('required'), 'color: ' + randomColor);
-      console.log('%cClass: ' + elemento.getAttribute('class'), 'color: ' + randomColor);
-      console.log('%cType: ' + elemento.getAttribute('type'), 'color: ' + randomColor);
-      console.groupEnd();
-      elemento.style.border = '3px solid ' + randomColor;
-  });
-  function getRandomColor() {
-      var letters = 'ABCDEF';
-      var color = '#';
-      for (var i = 0; i < 6; i++) {
-          color += letters[Math.floor(Math.random() * 6)];
-      }
-      return color;
-  }
-  var cookies = document.cookie.split('; ');
-  cookies.forEach(function(cookie) {
-      var [key, value] = cookie.split('=');
-      if (document.getElementById(key) !== null) {
-          document.getElementById(key).value = value;
-      }
-  });
-  elementos.forEach(function(elemento) {
-      elemento.addEventListener('click', function(event) {
-          event.stopPropagation();
-          var id = elemento.getAttribute('id');
-          var copyText = document.createElement('input');
-          copyText.type = 'text';
-          copyText.value = id;
-          document.body.appendChild(copyText);
-          copyText.select();
-          document.execCommand('copy');
-          document.body.removeChild(copyText);
-          console.clear();
-          console.log('ID copiado: ' + id);
-      });
-  });
-  elementos.forEach(function(elemento) {
-      if (elemento.style.display === 'none' || elemento.style.visibility === 'hidden') {
-          elemento.style.display = 'block';
-          elemento.style.visibility = 'visible';
-      }
-  });
+    var elementos = document.querySelectorAll('input, select, button');
+    elementos.forEach(function(elemento) {
+        var randomColor = getRandomColor();
+        console.groupCollapsed('%c' + elemento.getAttribute('id'), 'color: ' + randomColor);
+        console.log('%cId: ' + elemento.getAttribute('id'), 'color: ' + randomColor);
+        console.log('%cValue: ' + elemento.getAttribute('value'), 'color: ' + randomColor);
+        console.log('%cRequired: ' + elemento.getAttribute('required'), 'color: ' + randomColor);
+        console.log('%cClass: ' + elemento.getAttribute('class'), 'color: ' + randomColor);
+        console.log('%cType: ' + elemento.getAttribute('type'), 'color: ' + randomColor);
+        console.groupEnd();
+        elemento.style.border = '3px solid ' + randomColor;
+    });
+
+    function getRandomColor() {
+        var letters = 'ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 6)];
+        }
+        return color;
+    }
+    var cookies = document.cookie.split('; ');
+    cookies.forEach(function(cookie) {
+        var [key, value] = cookie.split('=');
+        if (document.getElementById(key) !== null) {
+            document.getElementById(key).value = value;
+        }
+    });
+    elementos.forEach(function(elemento) {
+        elemento.addEventListener('click', function(event) {
+            event.stopPropagation();
+            var id = elemento.getAttribute('id');
+            var copyText = document.createElement('input');
+            copyText.type = 'text';
+            copyText.value = id;
+            document.body.appendChild(copyText);
+            copyText.select();
+            document.execCommand('copy');
+            document.body.removeChild(copyText);
+            console.clear();
+            console.log('ID copiado: ' + id);
+        });
+    });
+    elementos.forEach(function(elemento) {
+        if (elemento.style.display === 'none' || elemento.style.visibility === 'hidden') {
+            elemento.style.display = 'block';
+            elemento.style.visibility = 'visible';
+        }
+    });
 }
 
